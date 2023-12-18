96e78e2e614843f949d08c80fba64e6a332380e7
nour-nono <115364839+nour-nono@users.noreply.github.com>
2023-12-18 12:51:09 +0200

    removed line #include"1-insertion_sort.c" in file 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,5 +1,39 @@
 #include "sort.h"
-#include "1-insertion_sort_list.c"
+
+/**
+ * swap_nodes - swap 2 nodes
+ *
+ * @node1: 1st node
+ * @node2: 2nd node
+ */
+
+void swap_nodes(listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	if (node2->next)
+		node2->next->prev = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+}
+
+
+/**
+ * swap_and_print - swap 2 nodes then print the linked list
+ *
+ * @node1: 1st node
+ * @node2: 2nd node
+ * @head: the head of the linked list
+ */
+void swap_and_print(listint_t *node1, listint_t *node2, listint_t **head)
+{
+	swap_nodes(node1, node2);
+	while ((*head)->prev)
+		(*head) = (*head)->prev;
+	print_list(*head);
+}
 
 /**
  * go - it's a round of bubble sort algorithm
