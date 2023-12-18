5911099463c920ed6188d292ec57d03a287908c2
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-12-19 01:43:19 +0200

    a function that sorts a deck of cards.
    Prototype: void sort_deck(deck_node_t **deck);
    You are allowed to use the C standard library function qsort
    
    ou have to push you deck.h header file, containing the previous data structures definition
    Each node of the doubly linked list contains a card that you cannot modify. You have to swap the nodes.
    You can assume there is exactly 52 elements in the doubly linked list.
    You are free to use the sorting algorithm of your choice
    The deck must be ordered:
    From Ace to King
    From Spades to Diamonds

---
+++ deck.h
@@ -0,0 +1,55 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * enum kind_e - Playing card suits
+ *
+ * @SPADE: Spade playing card suit
+ * @HEART: Heart playing card suit
+ * @CLUB: Club playing card suit
+ * @DIAMOND: Diamond playing card suit
+ */
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+/* ====================== Sort deck Function ====================== */
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
